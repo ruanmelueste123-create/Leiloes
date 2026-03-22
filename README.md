82618265cc4fd0340826bc689c3a4573600a5aba
Ruan-create <ruanmelueste123@gmail.com>
2026-03-21 22:51:58 -0400

    Projeto inicial

---
+++ README.md
@@ -0,0 +1,11 @@
+# Sistema de Leilões
+
+## Descrição
+Este projeto consiste em um sistema informatizado para uma casa de leilões. 
+O objetivo é auxiliar no gerenciamento de produtos, lances e usuários dentro da plataforma de leilões.
+
+## Tecnologias utilizadas
+- Java
+- MySQL
+- Git
+- GitHub
---
+++ src/conectaDAO.java
@@ -0,0 +1,41 @@
+
+import com.mysql.jdbc.PreparedStatement;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import javax.swing.JOptionPane;
+
+
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author Adm
+ */
+public class conectaDAO {
+
+
+    static PreparedStatement prepareStatement(String sql) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    
+    public Connection connectDB(){
+        Connection conn = null;
+        
+        try {
+        
+            conn = DriverManager.getConnection("jdbc:mysql://localhost/uc11?user=root&password=");
+            
+        } catch (SQLException erro){
+            JOptionPane.showMessageDialog(null, "Erro ConectaDAO" + erro.getMessage());
+        }
+        return conn;
+    }
+    
+}
